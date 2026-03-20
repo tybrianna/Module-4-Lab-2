e8fccc884d8a448012156393f38130bd38468ed8
tybrianna <tybriannahall@gmail.com>
2026-03-20 09:23:01 -0400

    added seperate sections for each task

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Task One
 let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
 let submitButton = document.getElementById("submitButton");
+
+// Task Two
+// Task Three
+// Task Four
